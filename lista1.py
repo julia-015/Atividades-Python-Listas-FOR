0fcf4cc1e78d456d09aabf9d5fed0abfbe9b2042
julia-015 <136704485+julia-015@users.noreply.github.com>
2023-07-25 09:58:38 -0300

    Add files via upload

---
+++ lista1.py
@@ -0,0 +1,21 @@
+import os
+
+lista = [3, 4, 5]
+
+tuple = (3, 4, 5)
+
+# dicionário = ["chave1": "valor1", 
+#               "chave2": "valor2"]
+
+# EXEMPLO
+
+part1 = [10, 11, 12, 13, 89]
+
+print(part1 [2])
+
+matriz = [[1, 2, 3], 
+          [4, 5, 6]]
+
+print(matriz [0] [1])
+
+os.system("pause")
